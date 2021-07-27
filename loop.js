8302641ea242b20dff00f17ba19744cf80d74eaf
me-sajib <me.mrsajib@gmail.com>
2021-07-27 21:18:19 +0600

    good coding, and pratice so cool

---
+++ loop.js
@@ -0,0 +1,13 @@
+const cars = ["BMW", "AUDI", "HINO", "NOHA"];
+let diff = "";
+for(let x of cars){
+    diff += x + " ";
+}
+console.log(diff);
+
+const string = "Javascript";
+let text = "";
+for(let x of string){
+    text += x + " ";
+}
+console.log(text);
